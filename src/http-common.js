8b11aed822612877649f9e7c4cb916e853a37700
Kan Yamamoto <kanyamamoto77@gmail.com>
2021-01-14 19:28:26 -0800

    Updated axios baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://divyup.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
